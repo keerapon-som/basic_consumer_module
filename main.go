686653b79fae9871786149b122a29e7ea7f9af68
Keerapon <datakeerapon@gmail.com>
2024-12-30 09:40:20 +0700

    fixbug

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	// var workerza consumer.ConsumerService
-	consumer.NewConsumer().ConsumerType("test").Handler(Testeiei).Open()
-	// workerza.Close()
+	workerza := consumer.NewConsumer().ConsumerType("test").Handler(Testeiei).Open()
+	// consumer.NewConsumer().ConsumerType("test").Handler(Testeiei).Open()
+	workerza.Close()
 	for {
 		fmt.Println("Just Sleep")
 		time.Sleep(10 * time.Second)
